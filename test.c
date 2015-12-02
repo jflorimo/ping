585d2d7e0a61f8c7dec73f55ac981014b08b012a
root <root@3a8df620ddd7.(none)>
2015-12-02 13:55:31 +0000

    fwe

--- test.c
+++ test.c
@@ -18,13 +18,10 @@ static char *hostname = NULL;
 static unsigned short calcsum(unsigned short *buffer, int length)
 {
 	unsigned long sum;
-
 	for (sum=0; length>1; length-=2)
 		sum += *buffer++;
-
 	if (length==1)
 		sum += (char)*buffer;
-
 	sum = (sum >> 16) + (sum & 0xFFFF);
 	sum += (sum >> 16);
 	return ~sum;
@@ -32,76 +29,78 @@ static unsigned short calcsum(unsigned short *buffer, int length)
 
 static void noresp(int ign)
 {
-  printf("No response from %s\n", hostname);
-  exit(0);
+	printf("No response from %s\n", hostname);
+	exit(0);
 }
 
-static void ping(const char *host)
+static void ping(char *host)
 {
-  struct hostent *h;
-  struct sockaddr_in pingaddr;
-  struct icmp *pkt;
-  int pingsock, c;
-  char packet[DEFDATALEN + MAXIPLEN + MAXICMPLEN];
+	struct addrinfo *addrinfo;
+	struct sockaddr_in pingaddr;
+	struct icmp *pkt;
+	int pingsock, c;
+	char packet[DEFDATALEN + MAXIPLEN + MAXICMPLEN];
 
-  if ((pingsock = socket(AF_INET, SOCK_RAW, 1)) < 0) {       /* 1 == ICMP */
-    perror("ping: creating a raw socket");
-    exit(1);
-  }
-  /* drop root privs if running setuid */
-  setuid(getuid());
-  memset(&pingaddr, 0, sizeof(struct sockaddr_in));
-  pingaddr.sin_family = AF_INET;
+	if ((pingsock = socket(AF_INET, SOCK_RAW, 1)) < 0)
+	{
+		perror("ping: creating a raw socket");
+		return ;
+	}
 
-  if (!(h = gethostbyname(host))) {
-    fprintf(stderr, "ping: unknown host %s\n", host);
-    exit(1);
-  }
-  memcpy(&pingaddr.sin_addr, h->h_addr, sizeof(pingaddr.sin_addr));
-  hostname = h->h_name;
+	memset(&pingaddr, 0, sizeof(struct sockaddr_in));
+	pingaddr.sin_family = AF_INET;
 
-  pkt = (struct icmp *) packet;
-  memset(pkt, 0, sizeof(packet));
-  pkt->icmp_type = ICMP_ECHO;
-  pkt->icmp_cksum = calcsum((unsigned short *) pkt, sizeof(packet));
+	if (getaddrinfo(host, NULL, NULL, &addrinfo) != 0)
+	{
+		printf("we got an error getting adress info!\n");
+		return ;
+	}
 
-  c = sendto(pingsock, packet, sizeof(packet), 0,
-             (struct sockaddr *) &pingaddr, sizeof(struct sockaddr_in));
+	pkt = (struct icmp *) packet;
+	memset(pkt, 0, sizeof(packet));
+	pkt->icmp_type = ICMP_ECHO;
+	pkt->icmp_cksum = calcsum((unsigned short *) pkt, sizeof(packet));
 
-  if (c < 0 || c != sizeof(packet)) {
-    if (c < 0)
-      perror("ping: sendto");
-    fprintf(stderr, "ping: write incomplete\n");
-    exit(1);
-  }
+	c = sendto(pingsock, packet, sizeof(packet), 0, addrinfo->ai_addr, addrinfo->ai_addrlen);
 
-  signal(SIGALRM, noresp);
-  alarm(2);                                     /* give the host 5000ms to respond */
-  /* listen for replies */
-  while (1) {
-    struct sockaddr_in from;
-    socklen_t fromlen = sizeof(from);
-    if ((c = recvfrom(pingsock, packet, sizeof(packet), 0,
-                      (struct sockaddr *) &from, &fromlen)) < 0) {
-      if (errno == EINTR)
-        continue;
-      perror("ping: recvfrom");
-      continue;
-    }
-    if (c >= 76) {                   /* ip + icmp */
-      struct iphdr *iphdr = (struct iphdr *) packet;
+	if (c < 0 || c != sizeof(packet)) {
+		if (c < 0)
+			perror("ping: sendto");
+		fprintf(stderr, "ping: write incomplete\n");
+    		return ;
+	}
 
-      pkt = (struct icmp *) (packet + (iphdr->ihl << 2));      /* skip ip hdr */
-      if (pkt->icmp_type == ICMP_ECHOREPLY)
-        break;
-    }
-  }
-  printf("%s is alive!\n", hostname);
-  return;
+	while (1) {
+		struct sockaddr_in from;
+		socklen_t fromlen = sizeof(from);
+		if ((c = recvfrom(pingsock, packet, sizeof(packet), 0,(struct sockaddr *) &from, &fromlen)) < 0)
+		{
+			if (errno == EINTR)
+				continue;
+			 perror("ping: recvfrom");
+			continue;
+		}
+		if (c >= 76)
+		{
+			struct iphdr *iphdr = (struct iphdr *) packet;
+			char *saddr = malloc(24 * sizeof(char));
+			char *daddr = malloc(24 * sizeof(char));
+			inet_ntop(AF_INET, &(iphdr->saddr), saddr, 24 * sizeof(char));
+			inet_ntop(AF_INET, &(iphdr->daddr), daddr, 24 * sizeof(char));
+			printf("TTL:%d from:%s to destination:%s\n", iphdr->ttl, saddr, daddr);
+
+			pkt = (struct icmp *) (packet + (iphdr->ihl << 2));
+			if (pkt->icmp_type == ICMP_ECHOREPLY)
+				 break;
+		}
+	}
+	printf("%s is alive!\n", hostname);
+	return;
 }
 
-int main ()
+int main (int ac, char**av)
 {
-  ping ("192.168.1.2");
-
+	hostname = av[1];
+	ping(av[1]);
+	return 0;
 }
